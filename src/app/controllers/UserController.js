90d043d9a4e96600e81fce3c6310f080c8b719f0
Carlos Augusto <carlosaugust.tech@gmail.com>
2021-02-05 16:16:45 -0300

    Upload of project Formvalidation.

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,37 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome é obrigatório!'),
+      email: Yup.string().email().required('Favor informe um email válido!'),
+      password: Yup.string().min(
+        6,
+        'A senha precisa ter no minímo 6 caracteres!'
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+}
+
+export default new UserController();
